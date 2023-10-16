8ec568d121acb345172f8b07525ba36cda1fdb3a
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-17 00:43:01 +0500

    Update Messages.jsx

--- frontend/src/components/chat/Messages.jsx
+++ frontend/src/components/chat/Messages.jsx
@@ -10,7 +10,6 @@ const Messages = () => {
     .filter(({ channelId }) => channelId === currentId);
   const channels = useSelector(channelsSelector.selectAll);
   const { t } = useTranslation();
-  const messagesEnd = useRef(null);
 
   const getCurrentChannel = () => {
     const currentChannel = channels.find(({ id }) => id === currentId);
@@ -34,12 +33,8 @@ const Messages = () => {
     ));
   };
 
-  useEffect(() => {
-    messagesEnd.current?.scrollIntoView();
-  }, [channelId]);
-
   return (
-    <div className="chat-messages-container">
+    <>
       <div className="bg-light mb-4 p-3 shadow-sm small">
         <p className="m-0">
           <b>{`# ${getCurrentChannel()}`}</b>
@@ -48,11 +43,13 @@ const Messages = () => {
           {t('chat.counter.count', { count: messages.length })}
         </span>
       </div>
-      <div id="messages-box" className="chat-messages overflow-auto px-5">
+      <div
+        id="messages-box"
+        className="chat-messages overflow-auto px-5 "
+      >
         {renderMessages()}
-        <div ref={messagesEnd} />
       </div>
-    </div>
+    </>
   );
 };
 
