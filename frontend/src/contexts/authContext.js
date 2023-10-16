49984a24135e7a9811c7488e0f587be51c9de333
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-16 20:12:48 +0500

    Update authContext.js

--- frontend/src/contexts/authContext.js
+++ frontend/src/contexts/authContext.js
@@ -5,7 +5,7 @@ const AuthContext = createContext({});
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(() => localStorage.getItem('user'));
   const logIn = (resData) => {
-    localStorage.setItem('user', JSON.stringify(resData));
+    localStorage.setItem('user', resData);
     setUser(resData);
   };
   const logOut = () => {
