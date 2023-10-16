7b7135b18accb38c73c6aa05e138f622ec60676a
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-16 20:32:19 +0500

    Update MessageForm.jsx

--- frontend/src/components/chat/MessageForm.jsx
+++ frontend/src/components/chat/MessageForm.jsx
@@ -7,14 +7,14 @@ import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import filter from 'leo-profanity';
 import useApi from '../../hooks/useApi.jsx';
-import useAuth from '../../hooks/useAuth.jsx';
+// import useAuth from '../../hooks/useAuth.jsx';
 // import AuthContext from '../../contexts/authContext';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MessageForm = () => {
   const rollbar = useRollbar();
   const currentId = useSelector((state) => state.channelsReducer.currentChannelId);
-  const { user: { username } } = useAuth();
+  // const { user: { username } } = useAuth();
   // const { currentUser } = useContext(AuthContext);
   const api = useApi();
 
@@ -28,7 +28,7 @@ const MessageForm = () => {
       const newMessage = {
         body: filter.clean(formik.values.text),
         channelId: currentId,
-        username,
+        username: 'admin',
       };
       try {
         await api.newMessage(newMessage);
