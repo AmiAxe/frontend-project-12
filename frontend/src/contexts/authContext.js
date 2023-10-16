2b51c54802d40e6831506a496abd77d6d708985a
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-16 20:06:14 +0500

    Update authContext.js

--- frontend/src/contexts/authContext.js
+++ frontend/src/contexts/authContext.js
@@ -3,7 +3,7 @@ import { createContext, useState, useMemo } from 'react';
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')));
+  const [user, setUser] = useState(() => localStorage.getItem('user'));
   const logIn = (resData) => {
     localStorage.setItem('user', JSON.stringify(resData));
     setUser(resData);
