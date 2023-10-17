4a68d803da31f68beba9262ae01ae290412fbd8f
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-17 12:35:01 +0500

    Update Channel.jsx

--- frontend/src/components/chat/Channel.jsx
+++ frontend/src/components/chat/Channel.jsx
@@ -26,7 +26,7 @@ const Channel = (props) => {
         <Dropdown className="w-100" as={ButtonGroup}>
           <Button
             variant={currentId === id ? 'secondary' : null}
-            onClick={() => dispatch(setCurrentChannelId(id))}
+            onClick={() => dispatch(setCurrentChannelId(id, currentUserId))}
             className="w-100 rounded-0 text-start text-truncate"
           >
             {`# ${name}`}
