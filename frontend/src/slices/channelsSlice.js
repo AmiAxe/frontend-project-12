5a2e1a2fcd81a953711f400e47bfdb8453ce8658
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-17 13:21:03 +0500

    Update channelsSlice.js

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -23,10 +23,6 @@ const channelsSlice = createSlice({
     },
     setCurrentChannelId: (state, { payload }) => {
       state.currentChannelId = payload;
-      const { id, userId } = payload;
-      if (userId === state.currentUserId) {
-        state.currentChannelId = id;
-      }
     },
   },
 });
