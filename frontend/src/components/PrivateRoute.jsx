2283e9ed25b86d468b39f55eb38f1908c9aa3539
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-18 17:50:33 +0500

    Update PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
-const initOutlet = () => {
+const InitOutlet = () => {
   const auth = useAuth();
   return auth.user ? <Outlet /> : <Navigate to={routes.loginPage()} />;
 };
 
-export default initOutlet;
+export default InitOutlet;
