e5c265d2ee953731dddc156f5a7e85bfbc02839a
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-19 00:03:14 +0500

    Update Messages.jsx

--- frontend/src/components/chat/Messages.jsx
+++ frontend/src/components/chat/Messages.jsx
@@ -36,7 +36,7 @@ const Messages = () => {
     if (messages.length > 0) {
       return (
         <>
-          {messages.map(({ body, channelId, username }) => (
+          {messages.map(({ body, id, username }) => (
             <div key={id} className="text-break mb-2">
               <b>
                 {username}
