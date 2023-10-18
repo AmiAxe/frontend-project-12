26f7f2ed66c1a4c4b667d6262284ca53efab3e8c
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-18 14:37:31 +0500

    Update authContext.js

--- frontend/src/contexts/authContext.js
+++ frontend/src/contexts/authContext.js
@@ -1,23 +1,33 @@
-import { createContext, useState, useMemo } from 'react';
+import { createContext, useState, useMemo, useEffect } from 'react';
 
 const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')));
+  const [user, setUser] = useState(() => localStorage.getItem('user'));
+
   const logIn = (resData) => {
-    localStorage.setItem('user', JSON.stringify(resData));
+    localStorage.setItem('user', resData);
     setUser(resData);
   };
+
   const logOut = () => {
     localStorage.removeItem('user');
     setUser(null);
   };
+
   const getAuthHeader = () => {
     if (user && user.token) {
       return { Authorization: `Bearer ${user.token}` };
     }
     return {};
   };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
 
   const memoValue = useMemo(
     () => ({
