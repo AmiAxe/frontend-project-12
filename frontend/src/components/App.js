57b5ce40e73d3af09f4053b4d5549736d45c7407
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-18 17:57:20 +0500

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import RegistrationForm from './forms/RegistrationForm';
 import AuthorisationForm from './forms/AuthorisationForm';
 import Chat from './chat/Chat';
-import PrivateRoute from './PrivateRoute';
+import InitOutlet from './PrivateRoute';
 import Navigation from './Navigation';
 import Page404 from './Page404';
 import { AuthProvider } from '../contexts/authContext';
@@ -23,14 +23,9 @@ const App = () => (
       <div className="d-flex flex-column h-100">
         <Navigation />
         <Routes>
-          <Route
-            path={routes.mainPage()}
-            element={
-              <PrivateRoute>
-                <Route path="" element={<Chat />} />
-              </PrivateRoute>
-            }
-          />
+          <Route path={routes.mainPage()} element={<InitOutlet />}>
+            <Route path="" element={<Chat />} />
+          </Route>
           <Route path={routes.signupPage()} element={<RegistrationForm />} />
           <Route path={routes.loginPage()} element={<AuthorisationForm />} />
           <Route path="*" element={<Page404 />} />
