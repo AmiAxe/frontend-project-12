e3ddb30eefde2b2f99d5961040d92354bd4f5fb5
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-20 14:30:20 +0500

    Update Add.jsx

--- frontend/src/components/popups/Add.jsx
+++ frontend/src/components/popups/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import _ from 'lodash';
 import { useFormik } from 'formik';
