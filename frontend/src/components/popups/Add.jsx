eb5d06807bfcc2c19d9ecfe03a42992313ec7d5d
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-20 11:43:16 +0500

    Update Add.jsx

--- frontend/src/components/popups/Add.jsx
+++ frontend/src/components/popups/Add.jsx
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { channelsSelector, setCurrentChannelId } from '../../slices/channelsSlice';
 import { hideModal } from '../../slices/modalsSlice';
 import useApi from '../../hooks/useApi';
-import store from '../../slices/index';
+// import store from '../../slices/index';
 
 const Add = () => {
   const rollbar = useRollbar();
@@ -49,10 +49,10 @@ const Add = () => {
         };
         try {
           await api.newChannel(newChannel);
-          store.dispatch(setCurrentChannelId(newChannel));
           dispatch(hideModal());
           toast.success(t('modals.add.toastText'));
           inputEl.current.focus();
+          dispatch(setCurrentChannelId(newChannel));
         } catch (err) {
           rollbar.error('addChannelError');
           if (!err.isAxiosError) {
