a3970e2de03dc60b9a5d394d361aa44f0ca6d926
Maksim Angelov <maxidrom.92@yandex.ru>
2023-10-20 11:06:22 +0500

    Update init.jsx

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -21,7 +21,6 @@ const init = async () => {
   socket.on('newMessage', (payload) => store.dispatch(addMessage(payload)));
   socket.on('newChannel', (payload) => {
     store.dispatch(addChannel(payload));
-    store.dispatch(setCurrentChannelId(payload.id));
   });
   socket.on('renameChannel', (payload) => {
     store.dispatch(updateChannel({ id: payload.id, changes: payload }));
